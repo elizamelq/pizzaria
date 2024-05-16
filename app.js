b8dba1a2ad81664738d14ea1e0a3632379a5270c
Juan Zimmerman de Souza Oliveira <juanito.tevez@gmail.com>
2024-05-16 13:12:10 -0300

    barra de pesquisa

---
+++ app.js
@@ -0,0 +1,24 @@
+/*
+document.addEventListener('DOMContentLoaded', function() {
+    const searchInput = document.getElementById('searchInput');
+    const imgDiv = document.getElementById('div__pizzas');
+    const imgs = imgDiv.querySelectorAll('.imagem__pizza');
+
+    searchInput.addEventListener('input', function(event) {
+        const searchTerm = event.target.value.toLowerCase();
+
+        imgs.forEach(function(img) {
+            const altText = img.getAttribute('alt').toLowerCase();
+            const imgLink = img.parentElement.getAttribute('href');
+
+            if (altText.includes(searchTerm)) {
+                img.style.display = 'inline-block';
+                img.parentElement.style.display = 'inline-block';
+            } else {
+                img.style.display = 'none';
+                img.parentElement.style.display = 'none';
+            }
+        });
+    });
+});
+*/
