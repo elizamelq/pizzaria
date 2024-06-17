fb66000b8e123cba484bde528bc2cf124b6dc32a
Juan Zimmerman de Souza Oliveira <juanito.tevez@gmail.com>
2024-06-17 17:27:00 -0300

    ajustes

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     const searchInput = document.getElementById('searchInput');
     const imgDiv = document.getElementById('div__pizzas');
@@ -18,6 +17,30 @@ document.addEventListener('DOMContentLoaded', function() {
                 img.style.display = 'none';
                 img.parentElement.style.display = 'none';
             }
-        });
-    });
+        });
+    });
+
+    // Seleciona todos os botões com a classe 'valor'
+    const valorButtons = document.querySelectorAll('.valor');
+    
+    valorButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            // Se o botão já está selecionado
+            if (this.classList.contains('clicked')) {
+                // Desmarca o botão clicado e restaura a cor original
+                this.classList.remove('clicked');
+                this.style.backgroundColor = 'var(--cor-tamanhos)';
+            } else {
+                // Remove a classe 'clicked' e restaura a cor original de todos os botões
+                valorButtons.forEach(btn => {
+                    btn.classList.remove('clicked');
+                    btn.style.backgroundColor = 'var(--cor-tamanhos)';
+                });
+
+                // Adiciona a classe 'clicked' e muda a cor do botão clicado
+                this.classList.add('clicked');
+                this.style.backgroundColor = '#FF5733'; // Nova cor
+            }
+        });
+    });
 });
